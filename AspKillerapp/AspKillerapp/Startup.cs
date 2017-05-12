12aa999bb4d2dbc8d75cb042ad53c11b541d5ee4
Willem Stas <328869@student.fontys.nl>
2017-05-12 12:44:47 +0200

    Made Project and Removed excess views

---
+++ AspKillerapp/AspKillerapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspKillerapp.Startup))]
+namespace AspKillerapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
